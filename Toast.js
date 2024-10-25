11d42d9bf8837f4c242664c9e8144bc84f0646c8
Ercan sahin <128996531+ercansahin16@users.noreply.github.com>
2024-10-25 18:22:47 +0300

    Update Toast.js

--- Toast.js
+++ Toast.js
@@ -1,19 +1,19 @@
 ﻿
 
 
-   // Formu AJAX ile gönderme fonksiyonu
+   
    function submitForm() {
       const form = document.getElementById('toastForm');
       const formData = new FormData(form);
 
-      // Form içindeki inputları kontrol et
+
       const isEmptyField = Array.from(form.elements).some(input => {
-         return input.required && !input.value.trim(); // Boş veya yalnızca boşluk ise
+         return input.required && !input.value.trim(); 
       });
 
       if (isEmptyField) {
          showToast('Tüm alanları doldurunuz! by❤️esahin', 'error');
-         return; // Boş alan varsa gönderme
+         return;
       }
 
       fetch(form.action, {
@@ -23,7 +23,7 @@
          .then(response => {
             if (response.ok) {
                showToast('Başarıyla kaydedildi! by❤️esahin', 'success');
-               form.reset(); // Formu temizle
+               form.reset(); 
             } else {
                showToast('Bir hata oluştu!', 'error');
             }
@@ -31,7 +31,7 @@
          .catch(() => showToast('Bir hata oluştu!', 'error'));
    }
 
-   // Toast gösterme fonksiyonu
+
    function showToast(message, type = 'success') {
       const toastContainer = document.getElementById('toastContainer');
       const toast = document.createElement('div');
@@ -44,6 +44,6 @@
       setTimeout(() => {
          toast.classList.remove('show');
          setTimeout(() => toast.remove(), 500);
-      }, 5000); // 5 saniye sonra toast kaybolur
+      }, 5000);
    }
 
